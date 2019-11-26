ab11847943e816629898588d7ad1ede70c6e89ad
bo johnson <bo@letterx.co>
2019-11-26 21:38:55 +0000

    first run through video

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header
+      :numCorrect="numCorrect"
+      :numTotal="numTotal"
+    />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+            v-if="questions.length && index < 10"
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+          />
+          <Score
+            v-if="questions.length && index >= 10"
+            :numCorrect="numCorrect"
+            :numTotal="numTotal"
+            :init="init"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
+<!-- v-if directive in QuestionBox above causes element not to render until the questions prop has been populated -->
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
+import Score from './components/Score.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox,
+    Score
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    }
+  },
+  methods: {
+    next() {
+      this.index++
+    },
+    increment(isCorrect) {
+      if(isCorrect) {
+        this.numCorrect++
+      }
+      this.numTotal++
+    },
+    init() {
+      console.log("resetting")
+      this.questions = []
+      this.index = 0
+      this.numCorrect = 0
+      this.numTotal = 0
+
+      fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', {
+        // https://opentdb.com/api_config.php
+      method: 'get'
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((jsonData) => {
+        this.questions = jsonData.results;
+      })
+    }
+  },
+  mounted: function() {
+    this.init()
   }
 }
 </script>
---
+++ src/components/Score.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <h2>Final Score:</h2>
+      <p><b>{{ numCorrect }}</b> out of {{ numTotal }}</p>
+      <b-button
+        @click="init" 
+        variant="info"
+      >
+        Try Again?
+      </b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    numCorrect: Number,
+    numTotal: Number,
+    init: Function
+  }
+}
+</script>
